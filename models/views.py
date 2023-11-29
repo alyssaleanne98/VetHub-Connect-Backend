df6570f48c78614637d777f42eb134179b0f492b
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-11-28 22:16:49 -0800

    index request to resources

--- models/views.py
+++ models/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Resources
 
 # Create your views here.
+
+def index(request):
+    resources = Resources.objects.all()
+    data = list(resources.values())
+    return JsonResponse(data, safe=False)
