b53e04e51b74ba9b91a668ede7293248ae752105
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-11-28 22:32:47 -0800

    resources returned to the frontend

--- vethub/urls.py
+++ vethub/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('my_app.urls'))
+    path('', include('my_app.urls')),
+    path('resources/', include('models.urls')),
 ]
