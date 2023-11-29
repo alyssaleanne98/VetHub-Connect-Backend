8430d40da41230b22357c7200af99497b0cbd2bb
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-11-28 17:58:43 -0800

    first commit

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vethub_connect.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vethub.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('<h1> hi there </h1>')
 
 # Create your views here.
