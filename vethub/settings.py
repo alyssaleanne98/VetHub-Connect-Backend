dcc76d0263cc595bcf7c240684845f2b39ee2c1c
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-12-07 08:58:06 -0800

    api set handle cors

--- vethub/settings.py
+++ vethub/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-w8a08rp(m00pq8frxr8$_si$k_wuie=u#od&v^&e@&db1x)mqf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True 
 
 
 # Application definition
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
 ]
 
 ROOT_URLCONF = 'vethub.urls'
